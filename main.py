62d4847cd0a7edbe80a5f158d7dd5e2eb2cb4458
lrezza <lrezza@outlook.com>
2024-05-07 13:00:26 +0200

    Add 3 block VGG

--- main.py
+++ main.py
@@ -17,22 +17,28 @@ def main():
 # run the test harness for evaluating a model
 def run_test_harness():
     # load dataset
-    (trainX, trainY), (testX, testY) = load_dataset()
+    (trainX, trainY), (validX, validY), (testX, testY) = load_dataset()
     
     # define model
     model = define_model()
     # fit model, should eventually get validation data from training data
-    history = model.fit(trainX, trainY, epochs=3, batch_size=64, validation_data=(testX, testY), verbose=1)
+    history = model.fit(trainX, trainY, epochs=10, batch_size=64, validation_data=(validX, validY), verbose=1)
     # evaluate model
-    _, acc = model.evaluate(testX, testY, verbose=0)
+    _, acc = model.evaluate(testX, testY, verbose=1)
     print('> %.3f' % (acc * 100.0))
     # learning curves
-    summarize_diagnostics(history)
+    summarize_diagnostics(history, "test_plot")
 
 def define_model():
     model = Sequential()
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(32, 32, 3)))
     model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
     model.add(MaxPooling2D((2, 2)))
     model.add(Flatten())
     model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
@@ -43,27 +49,26 @@ def define_model():
     return model
 
 # plot diagnostic learning curves
-def summarize_diagnostics(history):
+def summarize_diagnostics(history, filename):
     pyplot.figure(figsize=(7, 8))
     # plot loss
     pyplot.subplot(211)
     pyplot.title('Cross Entropy Loss')
     pyplot.plot(history.history['loss'], color='blue', label='train')
-    pyplot.plot(history.history['val_loss'], color='orange', label='test')
+    pyplot.plot(history.history['val_loss'], color='orange', label='validation')
     pyplot.legend()
     # plot accuracy
     pyplot.subplot(212)
     pyplot.title('Classification Accuracy')
     pyplot.plot(history.history['accuracy'], color='blue', label='train')
-    pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+    pyplot.plot(history.history['val_accuracy'], color='orange', label='validation')
     # save plot to file
-    filename = sys.argv[0].split('/')[-1]
     pyplot.legend()
-    pyplot.savefig(filename + '_plot.png')
+    pyplot.savefig(filename + ".png")
     pyplot.close()
 
 #load cifar10 dataset into a a train and test set
-def load_dataset():
+def load_dataset(valid_size=5000):
     (trainX, trainy), (testX, testy) = cifar10.load_data()
 
     trainY = to_categorical(trainy)
@@ -72,7 +77,15 @@ def load_dataset():
     trainX = normalize_data(trainX)
     testX = normalize_data(testX)
 
-    return (trainX, trainY), (testX, testY)
+    train_size = trainX.shape[0] - valid_size
+
+    validX = trainX[train_size:train_size + valid_size]
+    validY = trainY[train_size:train_size + valid_size]
+
+    trainX = trainX[:train_size]
+    trainY = trainY[:train_size]
+
+    return (trainX, trainY), (validX, validY), (testX, testY)
 
 #converts image data from unsigned ints in 0-255 to floats i 0-1 range
 def normalize_data(X):
